87eef9670d3160835546d7d40c5d4d571925d60c
Arek <abudkowski@interia.pl>
2021-09-04 09:45:54 +0200

    glutton_done

--- src/main/java/GLUTTON_Obzartuchy.java
+++ src/main/java/GLUTTON_Obzartuchy.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 
 class GLUTTON_Obzartuchy {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int numTests;
-        int invitedFatties;
-        int cookiesInOneBox;
+        int invitedFatties; // 1-10.000
+        int cookiesInOneBox; // 1-1.000.000.000
         int oneDayTimeSeconds = 86400;
+        int timeToEatOneCookie;
+        int totalCookiesAte;
         int boxesOfCookiesToBuy = 0;
 
         Scanner scanner = new Scanner(System.in);
         numTests = scanner.nextInt();
         scanner.nextLine();
         for (int i = 0; i < numTests; i++) {
+            totalCookiesAte = 0;
             invitedFatties = scanner.nextInt();
             cookiesInOneBox = scanner.nextInt();
             scanner.nextLine();
-            int[] invitedFattiesTable = new int[invitedFatties];
-            int[] timeToEatOneCookie = new int[invitedFatties];
             for (int j = 0; j < invitedFatties; j++) {
-                timeToEatOneCookie[j] = scanner.nextInt();
+                timeToEatOneCookie = scanner.nextInt();
                 scanner.nextLine();
+                int tempCookies = oneDayTimeSeconds / timeToEatOneCookie;
+                totalCookiesAte += tempCookies;
             }
+            if ((totalCookiesAte % cookiesInOneBox) == 0) {
+                boxesOfCookiesToBuy = totalCookiesAte / cookiesInOneBox;
 
+            } else {
+                boxesOfCookiesToBuy = (totalCookiesAte / cookiesInOneBox) + 1;
+            }
+            System.out.println(boxesOfCookiesToBuy);
         }
-
-
-        System.out.println(boxesOfCookiesToBuy);
     }
 }
