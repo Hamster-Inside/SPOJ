9ba43aad0b839435b6f689e0e8c3a07f776be0b2
Arek <abudkowski@interia.pl>
2021-09-21 19:43:41 +0200

    calc done

--- src/main/java/CALC.java
+++ src/main/java/CALC.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class CALC {
     public static void main(String[] args) {
-        
+        int a;
+        int b;
+        String operation;
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            operation = scanner.next();
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            if (operation.equals("+")) {
+                System.out.println(a+b);
+            } else if (operation.equals("-")) {
+                System.out.println(a-b);
+            }
+            else if (operation.equals("*")){
+                System.out.println(a*b);
+            }
+            else if (operation.equals("/")){
+                System.out.println(a/b);
+            }
+            else if (operation.equals("%")){
+                System.out.println(a%b);
+            }
+
+        }
     }
 }
