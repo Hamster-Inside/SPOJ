23196ab4ee827fde690ab3ce468b0ff09bae009b
Arek <abudkowski@interia.pl>
2021-09-28 18:42:12 +0200

    PP0604A done

--- src/main/java/PP0604A.java
+++ src/main/java/PP0604A.java
@@ -3,27 +3,43 @@ import java.util.Scanner;
 public class PP0604A {
     public static void main(String[] args) {
         int tests;
-      int n;
-      int sum = 0;
-      int simpleAverage = 0;
-      double average;
+        int n;
+        int sum = 0;
+        double val = 0;
+        double lowestVal = 100;
+        int result = 0;
+
+        double average;
 
 
+
+        // 1 2 3 4 --> 2
+        // 4 3 2 1 --> 3
+        // 1 9 40 55
 
 
         Scanner scanner = new Scanner(System.in);
         tests = scanner.nextInt();
         scanner.nextLine();
         for (int i = 0; i < tests; i++) {
+            lowestVal = 100;
+           sum = 0;
             n = scanner.nextInt();
             int[] numbers = new int[n];
-            for (int j = 0; j<n; j++){
+            for (int j = 0; j < n; j++) {
                 numbers[j] = scanner.nextInt();
-                sum+=numbers[j];
+                sum += numbers[j];
             }
-            simpleAverage = sum/n;
-            average = sum/n;
-
+            average = (double)sum / (double)n;
+           for (int j =0; j< n; j++){
+               val = Math.abs(numbers[j]-average);
+               if (val == lowestVal) continue;
+               if (val < lowestVal){
+                   lowestVal = val;
+                   result = numbers[j];
+               }
+           }
+            System.out.println(result);
         }
     }
 }
