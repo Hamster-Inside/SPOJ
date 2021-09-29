082f506c2f52444e5653aa889ee0a7def895e685
Arek <abudkowski@interia.pl>
2021-09-29 06:59:34 +0200

    JROWLIN v1.01

---
+++ src/main/java/JROWLIN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JROWLIN {
+    public static void main(String[] args) {
+
+        // ax+b+c, dane jest a,b,c
+        // brak rozwiazania = "BR"
+        // nieskonczenie wiele rozwiazan = "NWR"
+
+        double a;
+        double b;
+        double c;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+
+    }
+}
