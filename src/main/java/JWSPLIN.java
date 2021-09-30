5acc0b7a9136479f22195f8f4615843507c7401b
Arek <abudkowski@interia.pl>
2021-09-30 07:03:51 +0200

    JWSPLIN v1.01

---
+++ src/main/java/JWSPLIN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JWSPLIN {
+    public static void main(String[] args) {
+        int tests;
+        // y = ax+b
+
+        Scanner scanner = new Scanner(System.in);
+        tests = scanner.nextInt();
+        for (int i = 0; i<tests; i++){
+
+        }
+    }
+}
