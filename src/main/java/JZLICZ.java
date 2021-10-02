bd059bf39bc197e9770dfc7608a5cabe748e2da8
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2021-10-02 11:56:23 +0200

    JZLICZ done

--- src/main/java/JZLICZ.java
+++ src/main/java/JZLICZ.java
@@ -4,7 +4,7 @@ public class JZLICZ {
     public static void main(String[] args) {
         int texts;
         String allText = "";
-        int[] posCounter = new int[20];
+        int[] posCounter = new int[52];
         char[] alphabet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                                         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +17,13 @@ public class JZLICZ {
             for (int j = 0; j<alphabet.length; j++){
                 if (allText.charAt(i) == alphabet[j]){
                     posCounter[j]++;
+                    break;
                 }
+            }
+        }
+        for (int i =0; i< alphabet.length; i++){
+            if (posCounter[i] > 0){
+                System.out.println(alphabet[i] + " " + posCounter[i]);
             }
         }
 
