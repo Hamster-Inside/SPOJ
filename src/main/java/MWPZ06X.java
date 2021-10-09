53ababedad93ec7e3a325cfdaef95cc490cfdc45
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2021-10-09 17:11:00 +0200

    MWPZ06X done

---
+++ src/main/java/MWPZ06X.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MWPZ06X {
+    public static void main(String[] args) {
+        int liczbaZestawowDanych; // 1-500
+        int kroki;
+        Scanner scanner = new Scanner(System.in);
+        liczbaZestawowDanych = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < liczbaZestawowDanych; i++) {
+            kroki = scanner.nextInt();
+            System.out.println(Math.pow(kroki,2));
+        }
+    }
+}
