fcdd7fcc90149dd760418e9df70882123a9c985c
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2021-10-09 17:14:46 +0200

    BrainFuck done

---
+++ src/main/java/OSTBR_BrainFuck.java
@@ -0,0 +1,4 @@
+public class OSTBR_BrainFuck {
+    // I did it but I will not publish. You have to do it yourself :)
+    // I can help you but I will not give straight anwser ->You can contact me by mail admin@hamster-inside.pl
+}
