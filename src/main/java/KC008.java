a9f686bd5a42568e9499d0c0c567dc9bd00d75b9
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2021-10-12 20:35:41 +0200

    KC008 done

--- src/main/java/KC008.java
+++ src/main/java/KC008.java
@@ -4,14 +4,31 @@ public class KC008 {
     public static void main(String[] args) {
         long number;
         long sum=0;
+        long sumOfsums = 0;
+        String line;
+        String num = "";
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()){
-            number = scanner.nextLong();
-            sum+=number;
-            if (number == 0){
-                System.out.println(sum);
-                sum = 0;
+           line = scanner.nextLine();
+        
+            for (int i =0; i< line.length(); i++){
+                if (line.charAt(i) != ' '){
+                    num+=line.charAt(i);
+                    if (i == line.length()-1){
+                        sum+=Integer.parseInt(num);
+                        num = "";
+                    }
+                }
+                else {
+                    sum+=Integer.parseInt(num);
+                    num = "";
+                }
             }
+            System.out.println(sum);
+            sumOfsums+=sum;
+            sum = 0;
+
         }
+        System.out.println(sumOfsums);
     }
 }
