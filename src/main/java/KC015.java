a868a2ee6c7cc5f0173f1bc372c4610f0664e670
Arek <abudkowski@interia.pl>
2021-10-14 18:59:40 +0200

    KC015 v1.02

--- src/main/java/KC015.java
+++ src/main/java/KC015.java
@@ -4,17 +4,19 @@ public class KC015 {
     public static void main(String[] args) {
         String numString1;
         String numString2;
-        String newNum1;
-        String newNum2;
-        int num1;
-        int num2;
+        String numString1part;
+        String numString2part;
+        long num1 = 0;
+        long num2 = 0;
+        // 1 123123123131231232131231231312313123131313123123123123
+        // 34343434343434343433434334343343434 343434343434343434343434343434343434343
         String compareSign;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()){
             numString1 = scanner.next();
             compareSign = scanner.next();
             numString2 = scanner.next();
-            if (numString1.length() >= numString2){
+
             if (compareSign.equals("==")){
                 if (num1 == num2) System.out.println("1");
                 else System.out.println("0");
