574c75cabd3815ba62c44e2e4c99a959e2ac8ac9
Arek <abudkowski@interia.pl>
2021-10-15 06:25:25 +0200

    SIL done

--- src/main/java/SIL.java
+++ src/main/java/SIL.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class SIL {
     public static void main(String[] args) {
         long x1, y1, x2, y2, x3, y3, x4, y4;
-        long xCounter = 0;
-        long yCounter = 0;
+        long xCounter = -1;
+        long yCounter = -1;
         long p1, p2, p3;
 
         Scanner scanner = new Scanner(System.in);
@@ -22,12 +22,12 @@ public class SIL {
         else if (x1 == x3 && x2 == x4 && y1 == y3 && y2 == y4) System.out.println(p1);
         else {
             // count how many covered in x
-            for (long i = x1; i < x2; i++){
-                if (i > x3 && i <=x4) xCounter++;
+            for (long i = x1; i <= x2; i++){
+                if (i >= x3 && i <=x4) xCounter++;
             }
             // count how many covered in y
-            for (long i = y1; i< y2; i++){
-                if (i > y3 && i <=y4) yCounter++;
+            for (long i = y1; i<= y2; i++){
+                if (i >= y3 && i <=y4) yCounter++;
             }
 
 
