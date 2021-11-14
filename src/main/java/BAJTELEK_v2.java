79e816b341a8bab378fa044a5c52c0b8a2ffc9d1
Hamster-Inside <arkadiusz.budkowski.dev@gmail.com>
2021-11-14 15:25:39 +0100

    bajtelek v2 done

--- src/main/java/BAJTELEK_v2.java
+++ src/main/java/BAJTELEK_v2.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class BAJTELEK_v2 {
     public static void main(String[] args) {
         int numberOfDrawings;
+        long result;
+        double resultAreaOfGray, resultAreaOfBlack, resultBlack, resultGray;
         String inputPoints, emptyLine;
 
         Scanner scanner = new Scanner(System.in);
@@ -16,20 +18,33 @@ public class BAJTELEK_v2 {
                 if (!emptyLine.equals("")) break;
             }
             inputPoints = emptyLine;
-            int[] blackPointsTable = pointsTable(inputPoints);
+            long[] blackPointsTable = pointsTable(inputPoints);
             inputPoints = scanner.nextLine();
-            int[] grayPointsTable = pointsTable(inputPoints);
-            for (int k: blackPointsTable){
-                System.out.print(k + " ");
-            }
-
+            long[] grayPointsTable = pointsTable(inputPoints);
+            resultAreaOfBlack = countArea(blackPointsTable);
+            resultAreaOfGray = countArea(grayPointsTable);
+            resultBlack = resultAreaOfBlack*10;
+            resultGray = (resultAreaOfGray-resultAreaOfBlack)*6;
+            result = (long)(resultBlack+resultGray);
+            System.out.println(result);
 
         }
     }
 
-    static int[] pointsTable(String inputNums) {
+    static double countArea(long[] table){
+        double result = 0;
+        double sum = 0;
+        for (int i = 0; i<table.length-3;i+=2){
+            sum+=((table[i]+table[i+2])*(table[i+1]-table[i+3]));
+        }
+
+        result = 0.5*Math.abs(sum);
+        return result;
+    }
+
+    static long[] pointsTable(String inputNums) {
         String[] numbers = inputNums.split(" ");
-        int[] points = new int[numbers.length];
+        long[] points = new long[numbers.length];
         int i = 0;
         for (String n : numbers) {
             points[i++] = Integer.parseInt(n);
